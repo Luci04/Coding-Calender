6c4ca59bb9212d0ba1e1c51be69d39cc88cf5407
luci04 <avinashukla2000@gmail.com>
2021-12-16 23:38:47 +0530

    Request Done

--- script.js
+++ script.js
@@ -1,100 +1,238 @@
-
 const windowLocalSorage = window.localStorage;
-
+let in_24_contest = [];
+let currently_running_contest = [];
 var mappping = [];
-
+var All_data;
 var selctedSites = [];
+
+chrome.storage.sync.get(['selected_sites'], function (result) {
+    selctedSites = JSON.parse(result.selected_sites);
+});
+
+
+document.getElementById('next_24_hour').onclick = function () {
+
+    in_24_contest = collecting_24_hour_contest(All_data);
+
+    var element1 = document.getElementById('frame');
+    element1.style.display = "none"
+
+    var element2 = document.getElementById('contest-list');
+    element2.style.display = "block"
+
+    let ele = document.getElementById("contest-list");
+    console.log(in_24_contest)
+    var listing = "";
+
+    in_24_contest.map((e) => {
+        listing += default_structure(e);
+    })
+
+    setting_html(listing, ele);
+}
+
+document.getElementById("listing_sites").onclick = function () {
+    var element1 = document.getElementById('frame');
+    element1.style.display = "block"
+    var element2 = document.getElementById('contest-list');
+    element2.style.display = "none"
+    sites_list();
+}
+
+
+document.getElementById('running_contest').onclick = function () {
+
+    currently_running_contest = collecting_current_running_contest(All_data);
+
+    var element1 = document.getElementById('frame');
+    element1.style.display = "none"
+
+    var element2 = document.getElementById('contest-list');
+    element2.style.display = "block"
+
+    let ele = document.getElementById("contest-list");
+    console.log(currently_running_contest)
+    var listing = "";
+
+    currently_running_contest.map((e) => {
+        listing += default_structure(e);
+    })
+
+    setting_html(listing, ele);
+    console.log(listing)
+}
+
+
+
+
+
 
 // const Default_selected = windowLocalSorage.getItem("codher_calander");
 
-function handleClick(index) {
-    const temp = selctedSites.findIndex((ele) => ele === index);
 
-    if (temp != -1) {
-        selctedSites = selctedSites.filter((ele) => ele !== index);
-        return;
-    }
 
-    windowLocalSorage.setItem('selctedSites', selctedSites);
+function set_onClick_elevery_site(data) {
+    data.forEach((ele, index) => {
+        document.getElementById(ele[0]).onclick = function () {
+            const temp = selctedSites.findIndex((ele) => ele.Index === index);
+
+            if (temp != -1) {
+                selctedSites = selctedSites.filter((ele) => ele.Index !== index);
+
+                chrome.storage.sync.set({ 'selected_sites': selctedSites }, function () {
+                    console.log('Saved');
+                });
+                return;
+            }
+            // console.log(mappping[index]);
+            let obj = { "Index": index, "Site": mappping[index] }
+            selctedSites.push(obj)
+
+            chrome.storage.sync.set({ 'selected_sites': JSON.stringify(selctedSites) }, function () {
+                console.log('Saved');
+            });
+
+            console.log(selctedSites);
+        };
+    })
+
 }
 
 const sites_list = async () => {
+
+    document.getElementById("today_date").innerHTML = (new Date()).toLocaleDateString('en-GB');
+
     const response = await fetch('https://kontests.net/api/v1/sites')
+
+    if (!response) {
+        alert("fucked");
+    }
 
     const data = await response.json();
     console.log(data);
 
-    if (!response) {
-        return
-    }
 
     let ele = document.getElementById("todo-cmp__list");
 
     let listing = "";
-
+    console.log((selctedSites));
     data.map((e, index) => {
-        const temp = selctedSites.findIndex((ele) => ele === index);
+        var temp = selctedSites.find((ele) => ele.Index === index);
         listing +=
             `<li>
 				<label for="todo-${index}">
-					<input   id="todo-${index}" ${temp !== -1 ? 'checked' : ""}  type="checkbox">
-					<span onclick='handleClick(${index})' >
+					<input   id="todo-${index}" ${temp ? "checked" : ""}  type="checkbox">
+					<span id='${e[0]}'>
                         <a>${e[0]}</a>
                     </span>
 				</label>
 			</li>`
         mappping[index] = e[0];
     })
-    ele.innerHTML = listing
+
+    setting_html(listing, ele);
+    set_onClick_elevery_site(data);
+
 }
 
-const TimeDifference = (timeStart, timeEnd) => {
-    timeEnd = new Date(timeEnd)
-    timeStart = new Date(timeStart)
 
-    var diffHrs = timeEnd.getHours() - timeStart.getHours();
-    var diffMins = timeEnd.getMinutes() - timeStart.getMinutes();
+function collecting_24_hour_contest(data) {
+    data = filter_data_selected_sites(data);
+    let x = data.filter((e) => e.in_24_hours !== 'No')
+    return x;
+}
 
-    diffHrs += Math.round(diffMins / 60);
 
-    diffMins = diffMins % 60;
-    return diffHrs + ':' + diffMins;
-};
+function collecting_current_running_contest(data) {
+    data = filter_data_selected_sites(data);
+    let x = data.filter((e) => e.status === 'CODING')
+    return x;
+}
 
+function formatAMPM(date) {
+    date = new Date(date);
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+
+
+function default_structure(e) {
+    return `<li class="box">
+    <span></span>
+    <div class="title"><a target='_blank'  href="${e.url}">${e.name}</a> </div>
+    <div class="sub-title">contest at ${e.site}</div>
+    <div class="time">
+        <span>${formatAMPM(e.start_time)}</span>
+        <span>${formatAMPM(e.end_time)}</span>
+    </div>
+</li>`
+}
+
+
+function setting_html(data, ele) {
+    ele.innerHTML = data;
+}
+
+function filter_data_selected_sites(data) {
+    data = data.filter((e) => selctedSites.findIndex((ref) => ref.Site === e.site) !== -1);
+    return data;
+}
 
 const fetch_contest = async () => {
+
+    document.getElementById("frame").style.display = "none";
+
     const response = await fetch('https://kontests.net/api/v1/all')
 
     if (!response) {
-        return
+        return;
     }
-    const data = await response.json();
 
-    console.log(data);
+    All_data = await response.json();
 
-    let ele = document.getElementById("contest-list");
+    console.log(selctedSites.length);
+    if (selctedSites.length !== 0) {
+        var element2 = document.getElementById('contest-list');
+        element2.style.display = "block"
+        console.log(All_data);
 
-    let listing = "";
-    var time = new Date;
+        var element1 = document.getElementById('frame');
+        element1.style.display = "none"
 
-    data.map((e, index) => {
 
-        listing +=
-            `<li class="box">
-            <span></span>
-            <div class="title"><a target='_blank'  href="${e.url}">${e.name}</a> </div>
-            <div class="sub-title">contest at ${e.site}</div>
-            <div class="time">
-                <span>${e.start_time}</span>
-                <span>${e.end_time}</span>
-            </div>
-        </li>`
-        mappping[index] = e[0];
-    })
-    ele.innerHTML = listing
+        in_24_contest = collecting_24_hour_contest(All_data);
+
+        var element1 = document.getElementById('frame');
+        element1.style.display = "none"
+
+        var element2 = document.getElementById('contest-list');
+        element2.style.display = "block"
+
+        let ele = document.getElementById("contest-list");
+        console.log(in_24_contest)
+        var listing = "";
+
+        in_24_contest.map((e) => {
+            listing += default_structure(e);
+        })
+
+        setting_html(listing, ele);
+
+
+    } else {
+        document.getElementById("frame").style.display = "block";
+        sites_list();
+    }
+
+
 }
 
-
-
-// sites_list();
 fetch_contest();
+
